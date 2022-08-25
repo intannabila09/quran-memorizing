883f5b015ac38770d7cbff923bd2cd5a32ceb610
Arkun Rifki <arkunrifki17@gmail.com>
2022-08-25 19:38:40 +0700

    Layout halaman 599

--- src/assets/mushaf/juz30/index.js
+++ src/assets/mushaf/juz30/index.js
@@ -1,22 +1,26 @@
 export default Data = () => ({
-    id: 30,
-    metadata: require('assets/mushaf/juz30/content/data.json'),
-    pages: {
-        "20": {
-            image: require('assets/mushaf/juz30/pages/600.png'),
-            content: require('assets/mushaf/juz30/content/20.json'),
-        },
-        '21': {
-            image: require('assets/mushaf/juz30/pages/601.png'),
-            content: require('assets/mushaf/juz30/content/21.json')
-        },
-        '22': {
-            image: require('assets/mushaf/juz30/pages/602.png'),
-            content: require('assets/mushaf/juz30/content/22.json')
-        },
-        '23': {
-            image: require('assets/mushaf/juz30/pages/603.png'),
-            content: require('assets/mushaf/juz30/content/23.json')
-        },
-    }   
-})
+  id: 30,
+  metadata: require("assets/mushaf/juz30/content/data.json"),
+  pages: {
+    19: {
+      image: require("assets/mushaf/juz30/pages/599.png"),
+      content: require("assets/mushaf/juz30/content/19.json"),
+    },
+    20: {
+      image: require("assets/mushaf/juz30/pages/600.png"),
+      content: require("assets/mushaf/juz30/content/20.json"),
+    },
+    21: {
+      image: require("assets/mushaf/juz30/pages/601.png"),
+      content: require("assets/mushaf/juz30/content/21.json"),
+    },
+    22: {
+      image: require("assets/mushaf/juz30/pages/602.png"),
+      content: require("assets/mushaf/juz30/content/22.json"),
+    },
+    23: {
+      image: require("assets/mushaf/juz30/pages/603.png"),
+      content: require("assets/mushaf/juz30/content/23.json"),
+    },
+  },
+});
